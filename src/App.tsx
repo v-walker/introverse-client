2060c3c3cea38a7dd66f3c3c2308001d9ce457c6
Victoria Walker <vmsmith2014@gmail.com>
2021-12-26 17:21:09 -0500

    renamed file/component; sign-up form

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import BasicLargeCard from './common/pageComponents/basicLargeCard';
+import BasicLargeCard from './common/pageComponents/BasicLargeCard';
 
 /**
  * Landing page/entry point for app
@@ -12,13 +12,29 @@ let mainCardContent =
     <div className="col s12 m8 l8 center-align">
       <h1>Introverse</h1>
       <h6>Avoiding others, together</h6>
-      <hr />
+      
     </div>
     <div className="col s12 m4 l4">
       <h5>Sign Up</h5>
-      
+      <form className='col s12'>
+        <div className="input-field">
+          <input id="email" type="text" className="validate" />
+          <label htmlFor="email">Email</label>
+        </div>
+        <div className='input-field'>
+          <input id="homeCity" type="text" />
+          <label htmlFor="homeCity">Home City</label>
+        </div>
+        <div className='input-field'>
+          <input id="homeState" type="text" />
+          <label htmlFor="homeState">State</label>
+        </div>
+        <div className="input-field">
+          <input id="password" type="password" />
+          <label htmlFor="password">Password</label>
+        </div>
+      </form>
 
-      <h5>Log In</h5>
     </div>
   </div>
 </>
