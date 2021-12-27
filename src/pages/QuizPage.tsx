a9e7a42b48794c842f726da06b479184cef5603b
Victoria Walker <vmsmith2014@gmail.com>
2021-12-26 22:00:21 -0500

    quiz page basic setup

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import BasicLargeCard from '../common/pageComponents/BasicLargeCard';
+import QuizCardContent from '../common/pageComponents/QuizCardContent';
 
 function QuizPage() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <main className='container mt-5'>
+                <div className="row">
+                    <div className='col s12'>
+                        <h2>Quiz/About/Directions</h2>
+                        <p>Here is some sample text. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rem porro assumenda esse, odio aperiam corporis, excepturi maxime sunt, soluta nulla! Quis suscipit commodi repellendus deserunt ad alias sunt corporis.</p>
+                    </div>
+                    <div className='col s12 mt-5'>
+                        <BasicLargeCard cardContent={<QuizCardContent />} />
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
 
