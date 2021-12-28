7fddb22133604dc3b361f032f936b6a36f6c7bba
Victoria Walker <vmsmith2014@gmail.com>
2021-12-28 17:22:53 -0500

    footer layout responsive

--- src/common/layout/Footer.tsx
+++ src/common/layout/Footer.tsx
@@ -8,32 +8,36 @@ function Footer(): JSX.Element {
     return (
         <footer className='pt-5 pb-5'>
             <div className='row container'>
-                <div className='col s12 m4 left-align'>
+                <div className='col s12 m4 footer-col1'>
                     <div>Navigation Menu</div>
                     <br />
-                    <Link to="/">
-                        Home
-                    </Link>
-                    <br />
-                    <Link to="/quiz">
-                        Quiz
-                    </Link>
-                    <br />
-                    <Link to="/recommendations">
-                        Recommendations
-                    </Link>
-                    <br />
-                    <Link to="/about">
-                        About
-                    </Link>
-                    <br />
-                    <br />
-                    <a href="#top">Back to Top</a>
+                    <div>
+                        <Link to="/">
+                            Home
+                        </Link>
+                        <br />
+                        <Link to="/quiz">
+                            Quiz
+                        </Link>
+                        <br />
+                        <Link to="/recommendations">
+                            Recommendations
+                        </Link>
+                        <br />
+                        <Link to="/about">
+                            About
+                        </Link>
+                        <br />
+                        <br />
+                        <a href="#top">Back to Top</a>
+                        <br />
+                    </div>
                 </div>
-                <div className='col s12 m4 center-align'>
+                
+                <div className='col s12 m4 footer-col2'>
                     © Introverse 2021 | All rights reserved
                 </div>
-                <div className='col s12 m4 right-align'>
+                <div className='col s12 m4 footer-col3'>
                     <div>Interested in our Source Code?</div>
                     <br />
                     <a href="https://github.com/v-walker/introverse-client" target="blank">Github <VscGithub /></a>
