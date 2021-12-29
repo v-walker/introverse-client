def6f29fc36ec8f9f80ced8157f5394310e4da98
Victoria Walker <vmsmith2014@gmail.com>
2021-12-29 13:58:24 -0500

    more map setup

--- src/common/pageComponents/MapCardContent.tsx
+++ src/common/pageComponents/MapCardContent.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Map from '../../features/map/Map';
+// import Map from '../../features/map/Map';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 function MapCardContent(): JSX.Element {
+    
+    const render = (status: Status) => {
+        return <h1>{status}</h1>
+    }
+    
     return (
         <div className='map-card'>
-            <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true}> </Map>
+            {/* <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true}> </Map> */}
+            <Wrapper apiKey={`${process.env.REACT_APP_GMAPS_KEY}`} render={render}>
+                component here
+            </Wrapper>
         </div>
     )
 }
