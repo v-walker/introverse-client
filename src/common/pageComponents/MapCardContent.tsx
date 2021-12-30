2a200d80aba735ae34c7ddb42e633640ddc7a4a3
Victoria Walker <vmsmith2014@gmail.com>
2021-12-30 12:56:06 -0500

    working on geocoding; not working yet, but progress

--- src/common/pageComponents/MapCardContent.tsx
+++ src/common/pageComponents/MapCardContent.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Map from '../../features/map/Map';
-import Marker from '../../features/map/Marker';
+// import Marker from '../../features/map/Marker';
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+// Atlanta lat: 33.748995, lng:-84.387982
+
+// const geocoder = new google.maps.Geocoder();
+
+// const getGeocodeInfo = (locationString: string) => {
+    
+//     geocoder.geocode( { 'address': locationString}, function(results, status) {
+//         if (status === 'OK' && results) {
+//         return (results[0].geometry.location);
+//         } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//         }
+//     });
+// }
 
 function MapCardContent(): JSX.Element {
     const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
-    const [zoom, setZoom] = React.useState(3); // initial zoom
+    const [zoom, setZoom] = React.useState(14); // initial zoom
     const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
-        lat: 0,
-        lng: 0,
+        lat: 33.748995,
+        lng: -84.387982,
     });
 
     const onClick = (e: google.maps.MapMouseEvent) => {
@@ -26,15 +41,18 @@ function MapCardContent(): JSX.Element {
     const render = (status: Status) => {
         return <h1>{status}</h1>
     }
+
+    // useEffect(() => {
+    //     console.log(getGeocodeInfo("Atlanta, Georgia, USA"))
+    // }, [])
     
     return (
         <div className='map-card'>
-            {/* <Map mapType={google.maps.MapTypeId.ROADMAP} mapTypeControl={true}> </Map> */}
             <Wrapper apiKey={`${process.env.REACT_APP_GMAPS_KEY}`} render={render}>
                 <Map center={center} zoom={zoom} onClick={onClick} onIdle={onIdle} style={{ flexGrow: "1", height: "100%" }}>
-                    {clicks.map((latLng, i) => (
+                    {/* {clicks.map((latLng, i) => (
                         <Marker key={i} position={latLng} />
-                    ))}
+                    ))} */}
                 </Map>
             </Wrapper>
         </div>
