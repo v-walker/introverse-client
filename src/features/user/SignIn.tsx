5811a70dd2f9b550a6d2932131d71adac6a09b58
Andrew Hatch <89485988+AMHatch@users.noreply.github.com>
2022-01-04 11:13:18 -0500

    tuesday morning'

--- src/features/user/SignIn.tsx
+++ src/features/user/SignIn.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import React, {useEffect,FormEvent,useState } from 'react';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, userSelector, clearState, PayloadUserLoginInfo, } from './userSlice';
 import { useNavigate } from 'react-router-dom';
@@ -11,7 +11,7 @@ const SignIn = ({}) => {
 const dispatch = useDispatch();
 const navigate = useNavigate();
 
-const { handleSubmit } = useForm();
+// const { handleSubmit } = useForm();
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 
