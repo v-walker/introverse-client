f562f5bdae9d14ede4a6a39cf1ef0fb7e5eef146
Victoria Walker <vmsmith2014@gmail.com>
2022-01-05 11:33:50 -0500

    instructions write-up

--- src/pages/RecommendationsPage.tsx
+++ src/pages/RecommendationsPage.tsx
@@ -119,9 +119,26 @@ function RecommendationsPage() {
 
                         <Collapsible accordion popout>
 
-                            <CollapsibleItem icon={<>&nbsp;<GiDirectionSigns />&nbsp;</>} expanded={false} header="New here or looking for a refresher? Click here to view instructions on how to use this map." node="div">Instructions here</CollapsibleItem>
-                            <CollapsibleItem icon={<>&nbsp;<FaQuestion />&nbsp;</>} expanded={false} header="What does my introvert rating mean and how do I use it?" node="div">Instructions here</CollapsibleItem>
-                            <CollapsibleItem icon={<>&nbsp;<IoRocketSharp />&nbsp;</>} expanded={false} header="What if I would like to visit Mars?" node="div">Astronaut Mark Watney asks for your patience. This feature is not yet available.</CollapsibleItem>
+                            <CollapsibleItem icon={<>&nbsp;<GiDirectionSigns />&nbsp;</>} expanded={false} header="New here or looking for a refresher? Click here to view instructions on how to use this map." node="div">
+                                This page is where you will find the main functionality of this application. 
+                                <br /><br />
+                                <b>To find a location type</b>, select a place type from the drop-down menu under "Select Place Type" on the left-hand side in your browser or directly under your Intro-Safe shield on mobile view. Then press "search". Your map will populate markers for your sign-up location or the last locaiton you searched. 
+                                <br /><br />
+                                <b>To change your map search area</b>, you can either input a search location under "Change Search Location," then press the "search button." This input takes in a city name or zip code. Alternatively, you can pan to a new location on the map, then click or tap the map before pressing the  "set new map center" button above the map. Your place search will update with the new location selected.
+                                <br /><br />
+                                <b>To view details about a selected business or point of interest</b>, click on a map marker to view the title of the business/point of interest and the address. You will also see a link to "see times recommendations." You can either click this link or scroll down on the page to see your times recommendations chart for this location. Various popular times data is available for each day of the week. If there is no data available for this location, you will see "no data" in the chart section. If data is available, but you do not see anything on the chart, that location appears to be closed at that time/day. Try selecting another day of the week. 
+                                <br /><br />
+                                <b>To view more or less of the map</b>, use the "+" and "-" buttons on the bottom right-hand corner of the map to zoom in or out on your centered search area. 
+                                <br /><br />
+                                <b>What if I don't see any markers on the map?</b>, try zooming out. Your search type may be further away from your centered location than the map default zoom ratio. You can also try panning around your centered location. If there are no locations available in the "Map Locations List," we were unable to find any locations matching your search type for your selected map area.
+
+                            </CollapsibleItem>
+                            <CollapsibleItem icon={<>&nbsp;<FaQuestion />&nbsp;</>} expanded={false} header="What does my introvert rating mean and how do I use it?" node="div">
+                                Instructions here
+                            </CollapsibleItem>
+                            <CollapsibleItem icon={<>&nbsp;<IoRocketSharp />&nbsp;</>} expanded={false} header="What if I would like to visit Mars?" node="div">
+                                Astronaut Mark Watney asks for your patience. This feature is not yet available.
+                            </CollapsibleItem>
 
                         </Collapsible>
 
